681a4613be0daa838c61fa778c99815afb489a49
Kyle Bignell <kyle.bignell@gmail.com>
2023-05-22 15:40:19 +0100

    Added icon files

---
+++ src/components/icons/TerraformIcon.tsx
@@ -0,0 +1,9 @@
+export default function TerraformIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 40 40" version="1.1">
+            <g id="surface1">
+                <path style={{ stroke: 'none', fillRule: 'nonzero', fill: 'rgb(25.098039%,25.098039%,69.803922%)', fillOpacity: 1 }} d="M 23.789062 16.34375 L 23.789062 23.515625 L 30 19.933594 L 30 12.753906 Z M 23.789062 16.34375 " />
+                <path style={{ stroke: 'none', fillRule: 'nonzero', fill: 'rgb(36.078431%,30.588235%,89.803922%)', fillOpacity: 1 }} d="M 16.894531 12.753906 L 23.105469 16.34375 L 23.105469 23.515625 L 16.894531 19.925781 Z M 10 8.75 L 10 15.925781 L 16.210938 19.511719 L 16.210938 12.335938 Z M 16.894531 27.890625 L 23.105469 31.476562 L 23.105469 24.300781 L 16.894531 20.714844 Z M 16.894531 27.890625 " />
+            </g>
+        </svg>);
+}
